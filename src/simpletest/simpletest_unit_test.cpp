df0fa682be6a646735d1d24808452e30ef0234b5
unknown-0x <254237445+unknown-0x@users.noreply.github.com>
2026-04-16 22:32:13 +0700

    MSVC: Redirect CRT memory leak reports to stdout/stderr

--- src/simpletest/simpletest_unit_test.cpp
+++ src/simpletest/simpletest_unit_test.cpp
@@ -16,6 +16,9 @@ namespace simpletest {
 UnitTest::UnitTest() : reporter_{new ConsoleReporter()} {
 #ifdef _WIN32
   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
 #endif
 }
 
