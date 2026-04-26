a0016b1e2e8cd1f3ab53146f12b944a4a6739df5
unknown-0x <254237445+unknown-0x@users.noreply.github.com>
2026-04-26 22:40:46 +0700

    Update

--- src/simpletest/simpletest_unit_test.cpp
+++ src/simpletest/simpletest_unit_test.cpp
@@ -17,8 +17,8 @@ UnitTest::UnitTest() : reporter_{new ConsoleReporter()} {
 #ifdef _WIN32
   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
   _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
-  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+  (void)_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+  (void)_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
 #endif
 }
 
